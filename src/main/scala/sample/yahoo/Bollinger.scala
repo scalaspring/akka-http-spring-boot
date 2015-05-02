ffcf73ae6587f68cfd2d70443f70a238e3a1a12a
Lance Arlaus <lance.arlaus@gmail.com>
2015-05-02 08:17:46 -0400

    Interim statistics stream work

--- src/main/scala/sample/yahoo/Bollinger.scala
+++ src/main/scala/sample/yahoo/Bollinger.scala
@@ -1,9 +1,13 @@
 package sample.yahoo
 
-import akka.stream.stage.PushStage
+import akka.stream.stage.{SyncDirective, Context, PushStage}
 
-//class Bollinger[T <% Numeric](implicit num: Numeric[T]) extends PushStage[Statistics[T], ] {
-//
-//}
-//
-//case class BollingerPoints[T](lower: Double, middle: Double, upper: Double)
+class Bollinger extends PushStage[Statistics[_], BollingerPoints] {
+  override def onPush(elem: Statistics[_], ctx: Context[BollingerPoints]): SyncDirective = ctx.push(BollingerPoints(elem))
+}
+
+case class BollingerPoints(lower: Double, middle: Double, upper: Double)
+
+object BollingerPoints {
+  def apply(s: Statistics[_]): BollingerPoints = BollingerPoints(s.mean - 2 * s.stdDev, s.mean, s.mean + 2 * s.stdDev)
+}
--- src/main/scala/sample/yahoo/Statistics.scala
+++ src/main/scala/sample/yahoo/Statistics.scala
@@ -7,12 +7,13 @@ trait Statistics[T] {
   def variance: Double
   def stdDev: Double = Math.sqrt(variance)
 
-  def update(value: T): Statistics[T]
+  def apply(value: T): Statistics[T]
+  def apply(values: Iterable[T]): Statistics[T]
 }
 
 object Statistics {
 
-  def apply[T](population: Boolean = false)(implicit num: Numeric[T]): Statistics[T] = new StatisticsImpl[T](population)(num)
+  def apply[T](values: Iterable[T], population: Boolean = false)(implicit num: Numeric[T]): Statistics[T] = new StatisticsImpl[T](population)(num)(values)
 
   private case class StatisticsImpl[T](population: Boolean = false, M: Double = 0, S: Double = 0, k: Int = 1)(implicit num: Numeric[T]) extends Statistics[T] {
 
@@ -20,7 +21,7 @@ object Statistics {
     override def variance: Double = if (population) (S / (k-1)) else (S / (k-2))
 
     // See: http://stackoverflow.com/questions/895929/how-do-i-determine-the-standard-deviation-stddev-of-a-set-of-values
-    override def update(value: T): Statistics[T] = {
+    override def apply(value: T): Statistics[T] = {
       val x = num.toDouble(value)
       val newM = M + (x - M) / k
       val newS = (x - M) * (x - newM)
@@ -29,6 +30,7 @@ object Statistics {
       copy(M = newM, S = newS, k = newK)
     }
 
+    override def apply(values: Iterable[T]): Statistics[T] = values.foldLeft(this.asInstanceOf[Statistics[T]])((s, x) => s(x))
   }
 }
 
